0b96d70bfbcc3126cf594ed3d5f477aacd18ae24
wanderky <1689391053@qq.com>
2018-12-06 19:01:42 +0800

    spring配置优化
    spring配置优化

--- src/main/java/com/cy/eide/framework/config/WebConfig.java
+++ src/main/java/com/cy/eide/framework/config/WebConfig.java
@@ -1,7 +1,6 @@
 package com.cy.eide.framework.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
@@ -14,7 +13,6 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
  */
 @Configuration
 @EnableWebMvc
-@ComponentScan
 public class WebConfig extends WebMvcConfigurerAdapter {
     /**
      * JSP视图解析器.
